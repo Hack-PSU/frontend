ffd49981ac66813be4456593216160f815215377
Sushrut Shringarputale <sush.shring@gmail.com>
2017-12-29 10:34:51 -0800

    Before deploy should properly deploy to firebase now

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-echo $DEPLOY_FB
-if [[ $DEPLOY_FB -eq 0 ]]
+echo ${DEPLOY_FB}
+if [[ ${DEPLOY_FB} -eq 0 ]]
 then
     cd api;
     npm install;
-    DEPLOY_FB=1;
+    DEPLOY_FB=1
 fi
