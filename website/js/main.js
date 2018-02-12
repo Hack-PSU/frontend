0ef4ccff8589b8a1e5100c358d2b68aa1cd8c025
GITHUBFULLNAME <GITHUBEMAIL>
2018-02-12 10:19:52 -0500

    Navbar scroll highlight kinda works

--- website/js/main.js
+++ website/js/main.js
@@ -1,32 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.question').click(function() {
-    $(this).siblings().slideToggle('slow', 'linear');
-    $('p span', this).toggleClass('rotated');
-  });
+    $('.question').click(function () {
+        $(this).siblings().slideToggle('slow', 'linear');
+        $('p span', this).toggleClass('rotated');
+    });
 });
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+    $(window).on('scroll', function (e) {
+        var scrollTop = $(this).scrollTop();
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        if (scrollTop <= 20) {
+            $('.main-nav').find('li').each(function () {
+                $(this).removeClass('active');
+            })
+        } else {
+            $(".main-nav").find("li").each(function () {
+                var curLink = $(this).find("a").first();
+                var anchorEl = $(curLink.attr("href"));
+                if (anchorEl.offset().top <= scrollTop  && anchorEl.offset().top + anchorEl.height() > scrollTop) {
+                    $(this).addClass('active').siblings().removeClass('active');
+                }
+            });
+        }
+    });
+    $('.main-nav').find('a').click(function (e) {
+        e.preventDefault();
+        console.log($(e.target).attr('href'));
+        scrollToID($(e.target).attr('href'), 1000);
+    });
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1250, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+    function scrollToID(id, speed) {
+        var targetOffset = $(id).offset().top;
+        var mainNav = $('.main-nav');
+        navClicked = true;
+        $('html,body').animate({scrollTop: targetOffset}, speed);
+        setTimeout(function () {
+            navClicked = false;
+        }, speed);
+    }
 });
