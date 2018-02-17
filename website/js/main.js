1c8a4725e4f5b91b834134818c554aecccb83e2a
Sushrut Shringarputale <contact@sushshring.me>
2018-02-16 19:10:37 -0500

    A workable version

--- website/js/main.js
+++ website/js/main.js
@@ -4,30 +4,32 @@ $(document).ready(function () {
         $(this).siblings().slideToggle('slow', 'linear');
         $('p span', this).toggleClass('rotated');
     });
-});
-
-$(document).ready(function () {
     $(window).on('scroll', function (e) {
         var scrollTop = $(this).scrollTop();
 
-        if (scrollTop <= 20) {
-            $('.main-nav').find('li').each(function () {
-                $(this).removeClass('active');
-            })
-        } else {
-            $(".main-nav").find("li").each(function () {
-                var curLink = $(this).find("a").first();
-                var anchorEl = $(curLink.attr("href"));
-                if (anchorEl.offset().top <= scrollTop  && anchorEl.offset().top + anchorEl.height() > scrollTop) {
-                    $(this).addClass('active').siblings().removeClass('active');
-                }
-            });
-        }
+        // if (scrollTop <= 0) {
+        //     $('.main-nav').find('li').each(function () {
+        //         $(this).removeClass('active');
+        //     });
+        // } else {
+        $(".main-nav").find("li").each(function () {
+            var curLink = $(this).find("a").first();
+            var anchorEl = $(curLink.attr("href"));
+            if (anchorEl.offset().top <= scrollTop && anchorEl.offset().top + anchorEl.height() > scrollTop) {
+                $(this).addClass('active').siblings().removeClass('active');
+            }
+        });
+        // }
     });
-    $('.main-nav').find('a').click(function (e) {
+    $('.main-nav').find('li').click(function (e) {
         e.preventDefault();
-        console.log($(e.target).attr('href'));
-        scrollToID($(e.target).attr('href'), 1000);
+        scrollToID($(e.target).attr('href'), 500);
+    });
+
+    $('.animated-icon').hover(function () {
+        $(this).addClass('pulse');
+    }, function() {
+        $(this).removeClass('pulse');
     });
 
     function scrollToID(id, speed) {
