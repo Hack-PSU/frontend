ff4ff963a9d40df642cee193de55215a4c50ba96
Sushrut Shringarputale <contact@sushshring.me>
2018-02-23 18:54:25 -0500

    Added production environment configuration. Also updated travis.yml to work properly

--- user-registration-app/src/environments/environment.prod.ts
+++ user-registration-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCpvAPdiIcqKV_NTyt6DZgDUNyjmA6kwzU',
+    authDomain: 'hackpsu18.firebaseapp.com',
+    databaseURL: 'https://hackpsu18.firebaseio.com',
+    projectId: 'hackpsu18',
+    storageBucket: 'hackpsu18.appspot.com',
+    messagingSenderId: '1002677206617',
+  },
 };
