f1b6755c1881c9f5281cbef0ea82d8966c52bc3a
Sushrut Shringarputale <contact@sushshring.me>
2018-03-09 17:33:37 -0500

    - Retrieves live updates from localhost - Updates can expand and collapse now

--- user-registration-app/src/app/live-update/live-update.component.ts
+++ user-registration-app/src/app/live-update/live-update.component.ts
@@ -1,24 +1,110 @@
 import { Component, OnInit } from '@angular/core';
 import { LiveUpdatesService } from '../live-updates.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+
+declare var $: any;
+
+class UpdateModel {
+  public idLIVE_UPDATES: string;
+  public update_text: string;
+  public update_image: string;
+  public update_title: string;
+}
 
 @Component({
   selector: 'app-live-update',
   templateUrl: './live-update.component.html',
   providers: [LiveUpdatesService],
-  styleUrls: ['./live-update.component.css']
+  styleUrls: ['./live-update.component.css'],
 })
 export class LiveUpdateComponent implements OnInit {
+  static initialLoad = true;
+  static fullyCollapsed = false;
+  static fullyExpanded = false;
 
-  updates: string[];
+  updates: UpdateModel[];
+  idtoken: Observable<string>;
+  progress: { uploaded, total };
+  error: any;
+  loading = false;
 
-  constructor(public liveUpdates: LiveUpdatesService) {
+  static collapseListener(target: string) {
+    // initialLoad = false;
+    if (LiveUpdateComponent.fullyCollapsed) {
+      LiveUpdateComponent.fullyCollapsed = false;
+    }
+    if (LiveUpdateComponent.fullyExpanded) {
+      LiveUpdateComponent.fullyExpanded = false;
+    }
+    const $content = $('#' + target);
+    $content.slideToggle(500, () => {
+      // execute this after slideToggle is done
+      // change text of header based on visibility of content div
+    });
+  }
+
+  constructor(public liveUpdates: LiveUpdatesService, public afAuth: AngularFireAuth) {
     this.updates = [];
-    this.liveUpdates.getUpdates().subscribe((message: string) => {
-      this.updates.push(message);
+    this.liveUpdates.subject(new Event('connected'))
+      .subscribe(() => {
+        this.updates = [];
+        this.error = null;
+        this.progress = null;
+      });
+    this.liveUpdates.subject(new Event('disconnected'))
+      .subscribe(() => {
+        this.updates = [];
+        this.loading = true;
+      });
+    this.afAuth.auth.onAuthStateChanged((user) => {
+      this.idtoken = Observable.fromPromise(user.getIdToken(true));
+      this.idtoken.subscribe((value) => {
+        this.liveUpdates.getUpdates(value).subscribe((message: UpdateModel[]) => {
+          message.forEach(m => this.updates.unshift(m));
+
+        });
+      },                     (error) => {
+        this.error = error;
+      });
     });
   }
 
   ngOnInit() {
+  }
+
+  updateClicked(updateModel: UpdateModel) {
+    LiveUpdateComponent.collapseListener(`toggle_${updateModel.idLIVE_UPDATES}`);
+  }
+
+  expand() {
+    if (!LiveUpdateComponent.fullyExpanded) {
+      setTimeout(() => {
+        $('html, body').animate({
+          scrollTop: $('.update-container-row').last().offset().top,
+        },                      1000);
+      },         1000);
+      $('.expandable-update').each(function () {
+        $(this).show(1000);
+      });
+    }
+    LiveUpdateComponent.fullyCollapsed = false;
+    LiveUpdateComponent.fullyExpanded = true;
+  }
+
+  collapse() {
+    if (!LiveUpdateComponent.fullyCollapsed) {
+      $('html, body').animate({
+        scrollTop: $('.update-container-row').offset().bottom,
+      },                      2000);
+      setTimeout(() => {
+        $('.expandable-update').each(function () {
+          $(this).hide(1000);
+        });
+      },         1000);
+    }
+    LiveUpdateComponent.fullyCollapsed = true;
+    LiveUpdateComponent.fullyExpanded = false;
   }
 
 }
--- user-registration-app/src/app/live-updates.service.ts
+++ user-registration-app/src/app/live-updates.service.ts
@@ -2,21 +2,42 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs/Observable';
 import { AppConstants } from './AppConstants';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class LiveUpdatesService {
-  private url = AppConstants.API_BASE_URL + '/live/updates';
+  private url = 'http://localhost:5000/updates';
   private socket;
+
+  private broadcastSubject: BehaviorSubject<Event> = new BehaviorSubject<Event>(new Event(''));
+
+  public next(event: Event): void {
+    return this.broadcastSubject.next(event);
+  }
+
+  public subject(event: Event): Observable<Event> {
+    return this.broadcastSubject.asObservable().filter(e => e.type === event.type);
+  }
 
   constructor() { }
 
-  getUpdates() {
+  getUpdates(idtoken: string) {
     return new Observable((observer) => {
       this.socket = io(this.url, {
         path: '/v1/live',
+        transportOptions: {
+          polling: { extraHeaders: { idtoken } },
+        },
       });
-      this.socket.on('connection', () => {
+      this.socket.on('connect', () => {
         console.log('CONNECTED');
+        this.next(new Event('connected'));
+      });
+
+      this.socket.on('disconnect', () => {
+        console.log('DISCONNECTED');
+        this.next(new Event('disconnected'));
       });
       this.socket.on('update', (data) => {
         observer.next(data);
