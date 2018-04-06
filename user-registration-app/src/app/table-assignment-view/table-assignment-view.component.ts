3c1356786e1e516966cfc42c43b1d60f88b77be7
Andrew Tran <31971241+andrewltran39@users.noreply.github.com>
2018-04-06 16:02:13 -0400

    Table Assignment UI

--- user-registration-app/src/app/table-assignment-view/table-assignment-view.component.ts
+++ user-registration-app/src/app/table-assignment-view/table-assignment-view.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressValidator } from 'address-validator';
+import { HttpService } from '../HttpService';
+import { AppConstants } from '../AppConstants';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-table-assignment-view',
   templateUrl: './table-assignment-view.component.html',
   styleUrls: ['./table-assignment-view.component.css']
+//  providers: [HttpService],
 })
 export class TableAssignmentViewComponent implements OnInit {
 
-  constructor() { }
+  public travelForm: any;
+  public user: any;
+  public loading = false;
+  public errors = null;
+  public response = null;
+
+  constructor(private httpService: HttpService, private afAuth: AngularFireAuth, private router: Router) {
+    this.travelForm = {};
+  }
 
   ngOnInit() {
+    this.loading = true;
+    this.afAuth.auth.onAuthStateChanged((user) => {
+      this.loading = false;
+      if (!user) {
+        this.router.navigate([AppConstants.LOGIN_ENDPOINT]);
+      } else {
+        this.user = user;
+      }
+    });
+  }
+
+  onError() {
+    $('html, body').animate({
+      scrollTop: 0,
+    },                      1000);
+  }
+
+  fileAdded(event) {
+    this.travelForm.receipt = event.target.files;
+  }
+
+  onSubmit() {
+    console.log(this.travelForm);
+    this.loading = true;
+    this.httpService.submitTravelReimbursement(this.travelForm, this.user.uid)
+      .subscribe((value: any) => {
+        this.response = value.result;
+        this.loading = false;
+      },         (error: Error) => {
+        this.errors = error;
+        this.loading = false;
+      });
   }
 
 }
