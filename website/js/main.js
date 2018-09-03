90e7302a07dc76e24dab8fbc6bf4e7eedf52426b
Sushrut Shringarputale <sush.shring@gmail.com>
2018-09-02 20:11:55 -0400

    Added proof of concept for medium integration

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,12 @@
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 $(document).ready(function () {
+  var posts;
+  getMediumPosts(function(p) {
+    posts = p;
+    displayMediumPosts(posts);
+  });
 
   $(".button-collapse").sideNav();
   $('.slider').slider();
@@ -52,5 +60,63 @@ $(document).ready(function () {
     setTimeout(function () {
       navClicked = false;
     }, speed);
+  }
+
+  function getMediumPosts(callback) {
+    $.ajax({
+      url: 'https://medium.com/@hackpsu_team/latest?format=json',
+      converters: {
+        'text json': window.String,
+      },
+      success(data) {
+        var respJson = data.replace('])}while(1);</x>', '');
+        var obj = JSON.parse(respJson);
+        var post = obj.payload.references.Post;
+        if (post) {
+          var posts = Object.values(post).map(function(d) {
+            return Object.assign({}, {
+              title: d.title,
+              createdAt: d.createdAt,
+              subtitle: d.virtuals.subtitle,
+              image: d.virtuals.previewImage.imageId ?
+                'https://cdn-images-1.medium.com/max/800/'+d.virtuals.previewImage.imageId : null,
+              url: 'https://medium.com/@hackpsu_team/'+d.uniqueSlug
+            });
+          });
+          callback(posts);
+        }
+      },
+      error(data) {
+        console.log(data);
+      },
+    });
+  }
+
+  function displayMediumPosts(posts) {
+    posts.forEach(function (post) {
+      var date = new Date(post.createdAt);
+      var div =
+        '<div class="card">' +
+        '<div class="card-image"> ' +
+        '<img src=' + (post.image ? post.image : 'https://hackpsu.org/assets/images/logo-all-blue-no-padding.svg') + ' />' +
+          '<span class="card-title">' + post.title + '</span>' +
+          '</div>' +
+          '<div class="card-content">' +
+          '<p>' + post.subtitle + '</p>' +
+          '<div class="card-action">' +
+          '<div class="card-date">' +
+          '<span>' +
+          monthNames[date.getMonth()] + ' ' + new Date(post.createdAt).getDate() +
+          '</span>' +
+          '<span class="card-year">' + new Date(post.createdAt).getFullYear() + '</span>' +
+          '</div>' +
+          '<a class="card-link" target="_blank" href=' + post.url + '>' +
+          'Read on Medium' +
+          '</a>' +
+          '</div>' +
+          '</div>' +
+          '</div>';
+      $('.medium-post-viewer').append(div);
+    });
   }
 });
