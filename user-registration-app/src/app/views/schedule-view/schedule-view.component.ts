d849756ee34411479c14f2f67b4803721f3a30f9
Sushrut Shringarputale <sush.shring@gmail.com>
2018-09-20 13:57:44 -0400

    Schedule works! (#20)

--- user-registration-app/src/app/views/schedule-view/schedule-view.component.ts
+++ user-registration-app/src/app/views/schedule-view/schedule-view.component.ts
@@ -1,20 +1,7 @@
-
-import { from as observableFrom,  Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../../services/EventService/events.service';
-import { AppConstants } from '../../AppConstants';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Router } from '@angular/router';
-
-export class EventModel {
-  public uid: string;
-  public event_title: string;
-  public event_type: string;
-  public event_start_time: string;
-  public event_end_time: string;
-  public event_description: string;
-  public location_name: string;
-}
+import { CustomErrorHandlerService, HttpService } from "../../services/services";
+import { EventModel } from "../../models/event-model";
 
 @Component({
   selector: 'app-schedule-view',
@@ -29,63 +16,38 @@ export class ScheduleViewComponent implements OnInit {
   public activities: EventModel[];
   public meals: EventModel[];
   public workshops: EventModel[];
-  public error: any;
-  public progress: any;
-  public loading: boolean;
-  private idtoken: Observable<any>;
 
-  constructor(private afAuth: AngularFireAuth, private eventsService: EventsService, private _router: Router) {
-    this.eventsService.subject(new Event('connected'))
-      .subscribe(() => {
-        this.activities = [];
-        this.meals = [];
-        this.workshops = [];
-        this.error = null;
-        this.progress = null;
-      });
-    this.eventsService.subject(new Event('disconnected'))
-      .subscribe(() => {
-        this.activities = [];
-        this.meals = [];
-        this.workshops = [];
-        this.loading = true;
-      });
-    this.afAuth.auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.idtoken = observableFrom(user.getIdToken(true));
-        this.idtoken.subscribe((value) => {
-          this.eventsService.getEvents().subscribe((events: EventModel[]) => {
-            console.log(events);
-            events.forEach((m) => {
-              // if (new Date().getTime() < parseInt(m.event_end_time, 10)) {
-              switch (m.event_type) {
-                case 'activity':
-                  this.activities.push(m);
-                  break;
-                case 'food':
-                  this.meals.push(m);
-                  break;
-                case 'workshop':
-                  this.workshops.push(m);
-                  break;
-                default:
-                  break;
-              }
-              // }
-            });
-
-          });
-        },                     (error) => {
-          this.error = error;
-        });
-      } else {
-        this._router.navigate([AppConstants.LOGIN_ENDPOINT]);
-      }
-    });
+  constructor(private httpService: HttpService, private errorHandler: CustomErrorHandlerService) {
+    this.activities = [];
+    this.meals = [];
+    this.workshops = [];
   }
 
   ngOnInit() {
+    this.httpService.getEvents()
+      .subscribe((events: EventModel[]) => {
+        events
+          .filter(activity => parseInt(activity.event_end_time, 10) >= Date.now())
+          .forEach((event) => {
+            switch (event.event_type) {
+              case 'activity':
+                this.activities.push(event);
+                break;
+              case 'food':
+                this.meals.push(event);
+                break;
+              case 'workshop':
+                this.workshops.push(event);
+                break;
+              default:
+                break;
+            }
+          });
+      }, (error) => {
+        this.errorHandler.handleHttpError(error);
+      });
   }
+
 
   getStartTime(event: EventModel) {
     return new Date(parseInt(event.event_start_time, 10))
@@ -99,6 +61,7 @@ export class ScheduleViewComponent implements OnInit {
   getCurrentTime() {
     return new Date().getTime();
   }
+
   getEndTime(event: EventModel) {
     return new Date(parseInt(event.event_end_time, 10))
       .toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
