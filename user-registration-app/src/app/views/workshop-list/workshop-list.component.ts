c3d58d8cfb4ff47cf12ad2f0957387bbd99671b8
Sushrut Shringarputale <sush.shring@gmail.com>
2018-09-25 01:30:35 +0530

    Populated sponsorship section (#21)
    * Added day/night theming for sponsors
    * Added new sponsors

---
+++ user-registration-app/src/app/views/workshop-list/workshop-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workshop-list',
+  templateUrl: './workshop-list.component.html',
+  styleUrls: ['./workshop-list.component.css']
+})
+export class WorkshopListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
