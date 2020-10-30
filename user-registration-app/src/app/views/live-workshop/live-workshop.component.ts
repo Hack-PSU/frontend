764f3d55c5e4dafce28b2af3625e73b8ce317d8d
Paul Scott <8pscott@gmail.com>
2020-10-30 17:40:50 -0400

    updated live workshop component

--- user-registration-app/src/app/views/live-workshop/live-workshop.component.ts
+++ user-registration-app/src/app/views/live-workshop/live-workshop.component.ts
@@ -1,82 +1,37 @@
-import { Component, OnInit,Input } from '@angular/core';
-
-
+import { Component, OnInit, Input } from '@angular/core';
 declare var $: any;
 
 @Component({
   selector: 'app-live-workshop',
   templateUrl: './live-workshop.component.html',
   // providers: [LiveUpdatesService],
-  styleUrls: ['./live-workshop.component.css']
+  styleUrls: ['./live-workshop.component.css'],
 })
 
 export class LiveWorkshopComponent implements OnInit {
-    @Input() title: String;
-    @Input() descrip: String;
-    @Input() date: String;
-    @Input() time: String;
-    @Input() link: String;
-    @Input() presenter1img: String;
-    @Input() presenter1Name: String;
-    @Input() presenter2img: String;
-    @Input() presenter2Name: String;
-    @Input() presenter3img: String;
-    @Input() presenter3Name: String;
-    @Input() collapseID: String;
+  @Input() title: String;
+  @Input() description: String;
+  @Input() date: String;
+  @Input() time: String;
+  @Input() link: String;
+  @Input() presenter1img: String;
+  @Input() presenter1Name: String;
+  @Input() presenter2img: String;
+  @Input() presenter2Name: String;
+  @Input() presenter3img: String;
+  @Input() presenter3Name: String;
+  @Input() collapseID: String;
 
+  ngOnInit() {
+    this.removePresenter(1, this.presenter1Name, this.presenter1img)
+    this.removePresenter(2, this.presenter2Name, this.presenter2img)
+    this.removePresenter(3, this.presenter3Name, this.presenter3img)
+  }
 
-
-
-    constructor() {}
-
-    ngOnInit() {
-
-      if( !this.presenter3Name || !this.presenter3img){
-        var elem = document.getElementById("presenter3");
-        elem.parentNode.removeChild(elem);
-      }
-
-      if( !this.presenter2Name || !this.presenter2img){
-        var elem = document.getElementById("presenter2");
-        elem.parentNode.removeChild(elem);
-      }
-
-      if( !this.presenter1Name || !this.presenter1img){
-        var elem = document.getElementById("presenter1");
-        elem.parentNode.removeChild(elem);
-      }
-
-      var elementHeader = "#module #" + this.collapseID +".collapse:not(.show) {";
- 
-      // Create our stylesheet
-      var style = document.createElement('style');
-      style.innerHTML =
-        elementHeader +
-          'display: block;' +
-          'height: 4.6rem;' +
-          'overflow: hidden;' +
-        '}';
-
-      // Get the first script tag
-      var ref = document.querySelector('script');
-
-      // Insert our new styles before the first script tag
-      ref.parentNode.insertBefore(style, ref);
-
-
-      elementHeader = "#module #" + this.collapseID +".collapsing {";
-
-      // Create our stylesheet
-      var style = document.createElement('style');
-      style.innerHTML =
-        elementHeader +
-          'height: 4.6rem;' +
-        '}';
-
-      // Get the first script tag
-      var ref = document.querySelector('script');
-
-      // Insert our new styles before the first script tag
-      ref.parentNode.insertBefore(style, ref);
+  removePresenter(presenterNo: number, presenterName: String, presenterImg: String) {
+    if (!presenterName || !presenterImg) {
+      const elem = document.getElementById('presenter' + presenterNo);
+      elem.parentNode.removeChild(elem);
     }
+  }
 }
