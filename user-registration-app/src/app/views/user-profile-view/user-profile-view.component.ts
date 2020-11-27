9d2ec7236c6a5aee40edfd926e2a670f146992f1
Stan <stanleykwok61@gmail.com>
2020-11-27 18:34:24 -0500

    Refactored user-profile-view

--- user-registration-app/src/app/views/user-profile-view/user-profile-view.component.ts
+++ user-registration-app/src/app/views/user-profile-view/user-profile-view.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import app from 'firebase/app';
-import { AuthService } from '../../services/AuthService/auth.service';
-import { transition, trigger, useAnimation } from '@angular/animations';
-import { fadeInDown, fadeOutUp } from 'ng-animate';
 import { NgProgress } from 'ngx-progressbar';
 import { finalize, switchMap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../services/AuthService/auth.service';
 import { RegistrationApiResponse } from '../../models/registration';
 import { HttpService } from '../../services/HttpService/HttpService';
 import { Hackathon } from '../../models/hackathon';
@@ -14,57 +12,40 @@ import { Hackathon } from '../../models/hackathon';
   selector: 'app-user-profile-view',
   templateUrl: './user-profile-view.component.html',
   styleUrls: ['./user-profile-view.component.css'],
-  animations: [
-    trigger(
-      'fadeIn', [
-        transition(
-          ':enter',
-          useAnimation(fadeInDown, { params: { timing: 0.25 } }),
-        ),
-        transition(
-          ':leave',
-          useAnimation(fadeOutUp, { params: { timing: 0.25 } }),
-        ),
-      ],
-    ),
-  ],
 })
 export class UserProfileViewComponent implements OnInit {
   private static readonly DEFAULT_PROFILE_URL: string = '../../assets/icons/user.png';
-  private emailEditToggled: boolean;
-  private passwordEditToggled: boolean;
-  private nameEditToggled: boolean;
-  public currentPin: string;
-  registrations: RegistrationApiResponse[];
   currentHackathon: Hackathon;
+  currentPin = 'No active pin! Don\'t forget to register!';
 
   constructor(
-    public authService: AuthService,
+    private authService: AuthService,
     private httpService: HttpService,
     private progressService: NgProgress,
     private toastrService: ToastrService,
   ) { }
 
   ngOnInit() {
-    this.emailEditToggled = false;
-    this.passwordEditToggled = false;
-    this.nameEditToggled = false;
     const regObservable = this.httpService.getUserRegistrations()
-    .subscribe(
-      (registrations) => {
-        this.registrations = registrations;
-        regObservable.unsubscribe();
-        this.getCurrentPin();
-      },
-      ({ error }) => {
-        if (error.status === 404) {
-          this.toastrService.info('You have not registered for a hackathon yet. We could not find any data for those queries');
-        }
-      });
+      .subscribe(
+        (registrations: RegistrationApiResponse[]) => {
+          registrations.forEach((registration) => {
+            if (registration.hackathon.active) {
+              this.currentPin = registration.pin.toString();
+            }
+          })
+          regObservable.unsubscribe();
+        },
+        ({ error }) => {
+          if (error.status === 404) {
+            this.toastrService.info('You have not registered for a hackathon yet. We could not find any data for you');
+          }
+        },
+      );
     const hackObservable = this.httpService.getHackathons()
       .subscribe(
-        (hackathons) => {
-          hackathons.forEach((hackathon: Hackathon) => {
+        (hackathons: Hackathon[]) => {
+          hackathons.forEach((hackathon) => {
             if (hackathon.active) {
               this.currentHackathon = hackathon;
               hackObservable.unsubscribe();
@@ -75,48 +56,21 @@ export class UserProfileViewComponent implements OnInit {
           if (error.status === 404) {
             this.toastrService.info('No hackathons retrieved');
           }
-        })
-    this.currentPin = 'No active pin! Don\'t forget to register!';
-    this.getCurrentPin();
+        },
+      )
   };
 
-  getCurrentPin() {
-    this.registrations.forEach((registration) => {
-      if (registration.hackathon.active) {
-        this.currentPin = registration.pin.toString();
-      }
-    })
-  }
-
   public getUserPhotoUrl(user: app.User | null) {
-    if (user.photoURL) {
-      return user.photoURL;
-    }
-    return UserProfileViewComponent.DEFAULT_PROFILE_URL;
-  }
-
-  editEmail() {
-    this.emailEditToggled = true;
-  }
-
-  editPassword() {
-    this.passwordEditToggled = true;
-  }
-
-  editName() {
-    this.nameEditToggled = true;
+    return user.photoURL || UserProfileViewComponent.DEFAULT_PROFILE_URL;
   }
 
   submitNewName(value: string) {
     this.progressService.ref().start();
     const observable = this.authService.currentUser
       .pipe(
-        switchMap((user) => {
-          return user.updateProfile({ displayName: value, photoURL: user.photoURL });
-        }),
+        switchMap(user => user.updateProfile({ displayName: value, photoURL: user.photoURL })),
         finalize(() => {
           this.progressService.ref().complete();
-          this.nameEditToggled = false;
         }),
       ).subscribe(
         () => {
@@ -135,13 +89,8 @@ export class UserProfileViewComponent implements OnInit {
     this.progressService.ref().start();
     const observable = this.authService.currentUser
       .pipe(
-        switchMap((user) => {
-          return user.updateEmail(value);
-        }),
-        finalize(() => {
-          this.progressService.ref().complete();
-          this.nameEditToggled = false;
-        }),
+        switchMap(user => user.updateEmail(value)),
+        finalize(() => this.progressService.ref().complete()),
       ).subscribe(
         () => {
           this.toastrService.info('Successfully changed display name');
@@ -155,25 +104,20 @@ export class UserProfileViewComponent implements OnInit {
       );
   }
 
-  submitNewPassword(value1: string, value2: string) {
-    if (value1.length === 0 || value2.length === 0) {
+  submitNewPassword(newPassword: string, confirmedPassword: string) {
+    if (newPassword.length === 0 || confirmedPassword.length === 0) {
       this.toastrService.error('Password not entered');
       return;
     }
-    if (value1 !== value2) {
+    if (newPassword !== confirmedPassword) {
       this.toastrService.error('Passwords do not match');
       return;
     }
     this.progressService.ref().start();
     const observable = this.authService.currentUser
       .pipe(
-        switchMap((user) => {
-          return user.updatePassword(value1);
-        }),
-        finalize(() => {
-          this.progressService.ref().complete();
-          this.nameEditToggled = false;
-        }),
+        switchMap(user => user.updatePassword(newPassword)),
+        finalize(() => this.progressService.ref().complete()),
       ).subscribe(
         () => {
           this.toastrService.info('Successfully changed display name');
