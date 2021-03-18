35a5170cd24b0f396b83f8da0dbf85fa2326000c
Sujay Kandwal <sujaykandwal@gmail.com>
2021-03-17 21:39:56 -0700

    Added to code for counter to adjust to timezone

--- user-registration-app/src/environments/environment.prod.ts
+++ user-registration-app/src/environments/environment.prod.ts
@@ -14,9 +14,18 @@ export const environment = {
 
   // Note: Months are indexed at 0
   // TODO: Manually set this and push the day of hackathon because of time zone differences.
+  /*
   liveWebsiteGuardTime: new Date(2021, 2, 21, 17),
   hackathonStartTime: new Date(2021, 2, 19, 16),
   timerStartTime: new Date(2021, 2, 19, 17),
   hackathonEndTime: new Date(2021, 2, 21, 17),
+  rsvpStartTime: new Date(2019, 10),
+  */
+
+  //New code that accounts for timezone differences. Time is set for 5PM EST Friday
+  liveWebsiteGuardTime: new Date(Date.UTC(2021, 2, 12, 13)),
+  hackathonStartTime: new Date(Date.UTC(2021, 2, 19, 20)),
+  timerStartTime: new Date(Date.UTC(2021, 2, 19, 21)),
+  hackathonEndTime: new Date(Date.UTC(2021, 2, 21, 21)),
   rsvpStartTime: new Date(2019, 10),
 };
